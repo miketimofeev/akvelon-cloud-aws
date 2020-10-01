298b4a2f2f4471788a8e52925d09b2ebe886a734
Mikhail Timofeev <v-mitim@microsoft.com>
2020-10-01 22:53:50 +0300

    commit ha

--- iac/cloudformation/wordpress/ha/run.sh
+++ iac/cloudformation/wordpress/ha/run.sh
@@ -1,1 +1,1 @@
-aws cloudformation create-stack --stack-name mystack --disable-rollback --template-body file://wordpress_template.yaml --cli-input-json file://input.json
+aws cloudformation --region us-west-2 create-stack --stack-name mystack-ha --disable-rollback --template-body file://wordpress_template.yaml --cli-input-json file://input.json
