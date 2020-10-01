40b61d004f4f1057a7612d8b8066dd3edf0e2dcc
Mikhail Timofeev <v-mitim@microsoft.com>
2020-10-01 22:26:05 +0300

    change to us-west-2

--- iac/cloudformation/wordpress/single/run.sh
+++ iac/cloudformation/wordpress/single/run.sh
@@ -1,1 +1,1 @@
-aws cloudformation create-stack --stack-name mystack-single --template-body file://wordpress_template.yaml --cli-input-json file://input.json
+aws cloudformation --region us-west-2 create-stack --stack-name stack-single --template-body file://wordpress_template.yaml --cli-input-json file://input.json
