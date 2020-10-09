4c0c486e235d7edd03af9c303698cb07a9185ce7
Mikhail Timofeev <v-mitim@microsoft.com>
2020-10-09 12:20:35 +0300

    add vars

--- iac/terraform/wordpress/ha/apply.sh
+++ iac/terraform/wordpress/ha/apply.sh
@@ -1,1 +1,1 @@
-terraform apply -var="vpc_id=vpc-e816318f" -var="ec2_keypair_name=wordpress_key_pair"
+terraform apply -var="vpc_id=vpc-17a5e46f" -var="ec2_keypair_name=wordpress_keypair"
---
+++ iac/terraform/wordpress/ha/vars.tf
@@ -0,0 +1,39 @@
+variable "ec2_keypair_name" {
+  type    = string
+  default = "wordpress_keypair"
+}
+
+variable "ec2_ami" {
+  type    = string
+  default = "ami-07a29e5e945228fa1" # ubuntu image
+}
+
+variable "ec2_instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "db_instance_type" {
+  type    = string
+  default = "db.t2.micro"
+}
+
+variable "db_username" {
+  type    = string
+  default = "user"
+}
+
+variable "db_password" {
+  type    = string
+  default = "abc123456"
+}
+
+variable "db_name" {
+  type    = string
+  default = "wordpress_db"
+}
+
+variable "db_allocated_storage" {
+  type = number
+  default = 5
+}
